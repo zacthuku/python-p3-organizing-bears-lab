4adde6bdeaae215891ec6569b7ae9f0ae2f1bd5a
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-20 19:54:48 +0300

    sql lab

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 5, 'M', 'black', 'aggressive', 1),
+('Tabitha', 7, 'F', 'white', 'sweet', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'gruff', 1),
+('Melissa', 4, 'F', 'gray', 'cheerful', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 0),
+('Wendy', 3, 'F', 'white', 'friendly', 1),
+(NULL, 2, 'M', 'black', 'mysterious', 1); -- unnamed bear
